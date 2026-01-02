9265a8c274efcf74ca7f686fac3603c42d851ffe
balaji-borude <balajiborude2503@gmail.com>
2026-01-02 17:21:48 +0530

    Completed the .substring() function in java (string concept)

--- String/string.java
+++ String/string.java
@@ -35,6 +35,48 @@ class string {
         }
         return true;
     }
+
+
+    // Q3. find the shortest path (displacement )
+    public static double ShortestPath(String path){
+        int x=0,y=0;
+
+        // time complexity == > O(n) - linear time 
+        for(int i=0; i<path.length();i++){
+            if(path.charAt(i)=='N'){
+                y++;
+            }
+            else if(path.charAt(i)=='S'){
+                y--;
+            }
+            else if(path.charAt(i)=='E'){
+                x++;
+            }
+            else{
+                x--;
+            }
+
+        }
+
+        int x2 = x*x;
+        int y2= y*y;
+
+        return  Math.sqrt(x2 + y2);
+
+
+    }
+
+
+    //Q4. 
+    public static String  PrintSubstring(String str, int si, int ei){
+        String substr = "";
+
+        for(int i=si; i<ei; i++){
+            substr = substr + str.charAt(i);
+        }
+        return substr;
+    }
+
     public static void main(String args[]) {
         System.out.println("hellow world");
 
@@ -48,10 +90,31 @@ class string {
         // Q2. palindrome string --
         // (racecar, noon, madam etc..)
 
-        String str = "racecar";
+        //String str = "racecar";
         //System.out.println(isPalindrome(str));
 
         // can we check palindrome no. with two pointer approach 
-        System.out.println(isPalindromeTwoPointerApp(str));
+        // System.out.println(isPalindromeTwoPointerApp(str));
+
+
+        // Q3. given  a route containig 4 directions (E,W,N,S) , find the shortest path to reach the destination 
+        //longest path --->  "WNEENESENNN"
+
+        // square root of (x2-x1)^2 +(y2-Y1)^2
+
+        String path = "WNEENESENNN";
+        System.out.println(ShortestPath(path));
+
+
+        //######## substring #######\
+
+        String str ="HelloWorld";
+        System.out.println(PrintSubstring(str, 0, 4));
+
+
+        // there is inbuild mehtod is available in java to get the Substring from string i.e --> .substring()
+
+        System.out.println(str.substring(0,3));
+
     }
 }
