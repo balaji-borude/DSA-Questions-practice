a3efde6117d88fa5cfef50767ab1411f8d81690e
balaji-borude <balajiborude2503@gmail.com>
2026-01-30 11:22:25 +0530

    Recursion is resume  after long time

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,11 +1,13 @@
 package Recursion;
 
+import java.io.PrintStream;
+
 public class Recursion {
 
     // factorial function
     public static int factorial(int num) {
         // base case
-        if (num == 1) {
+        if (num == 0) {
             return 1;
         }
         int res = num * factorial(num - 1);
@@ -13,21 +15,73 @@ public class Recursion {
     };
 
     // Print the value from 10 to 1 by recursion
-    public static void PrintDec(int num) {
-        if (num == 1) {
-            System.out.print(num + " ");
-            return;
+    public static int PrintDec(int num) {
+        // base case
+        if(num == 1) {
+            return 1;
         }
-        System.out.print(num + " ");
-        PrintDec(num - 1);
+         // reduction kel --> 
 
+        // number la Print kele + parat function call kele ;
+        return num + PrintDec(num);
     }
 
-    public static void main(String[] args) {
-        int result = factorial(5);
-        System.out.println(result);
+    // print number from n to 1 in (increaseing order)
+    public static void PrintInc(int num){
+        if(num ==1){
+            System.out.print(1 + " ");
+            return;
+        }
 
-        PrintDec(10);
+        PrintInc(num-1);
+
+        System.out.print(num + " ");
+    }
+    // Fibonacci number --> sum of prev ttwo number eg 2 ==> 1+0;
+    public static int Fibonacci(int num) {
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+
+        return Fibonacci(num - 1) + Fibonacci(num - 2);
+    }
+
+    // sum of n  natural number
+    public static int SumOfNaturals(int n){
+
+        if(n==1){
+            return 1;
+        };
+
+        int res = n + SumOfNaturals(n-1);
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        //int result = factorial(5);
+        // System.out.println(factorial(5));
+
+        // PrintDec(10);
+        //System.out.println(Fibonacci(6));
+        /*
+         * F(0) = 0
+         * F(1) = 1
+         * F(2) = 0 + 1 = 1
+         * F(3) = 1 + 1 = 2
+         * F(4) = 1 + 2 = 3
+         * F(5) = 2 + 3 = 5
+         * F(6) = 3 + 5 = 8
+         */
+
+
+        // Print increasing order
+        // int  num =10;
+        // PrintInc(num);
+
+        //sum of naturals 
+        System.out.println(SumOfNaturals(5));
     }
 
 }
