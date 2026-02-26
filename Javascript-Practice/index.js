b9f0ec469b43c318efd01926c0d5c2ef8b2d4fe9
balaji-borude <balajiborude2503@gmail.com>
2026-02-26 11:23:06 +0530

    Javascript Practice Concept (Promise)

--- Javascript-Practice/index.js
+++ Javascript-Practice/index.js
@@ -1,5 +1,6 @@
 // Basic Promise Example 
 
+// Promise is the object that represents the eventual completion (or failure) of an asynchronous operation, and its resulting value.
 
 // const promise = new Promise((resolve,reject)=>{
 //     const result = true;
@@ -13,11 +14,35 @@
 
 const promise = new Promise((resolve,reject)=>{
     resolve("Success");
-
-    
+   
 })
 
 .then(()=>{console.log("Promise Fulfilled ")})
 .catch(()=>{console.log("Promise Rejected ")});
 
-// console.log(promise);
+
+// ASync and await 
+
+async function test() {
+  return 5;
+};
+
+let result =   test();
+console.log(result); // it return Promise by default
+
+// but when we use await keyword it return the value directly
+let result2 = await test();
+console.log(result2);
+
+
+// use of await keyword
+async function getData() {
+  const result = await Promise.resolve("Hello");
+  console.log(result);
+}
+
+getData();
+
+
+// Debouncing 
+
