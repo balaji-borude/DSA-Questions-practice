4adf263262d0629ed2ff385a6006096ac9855818
balaji-borude <balajiborude2503@gmail.com>
2026-04-10 12:44:32 +0530

    completing Homework

---
+++ Home_work/homework.java
@@ -0,0 +1,8 @@
+package Home_work;
+
+public class homework {
+
+    public static void main(String [] args){
+        System.out.println("Working on the homework screenshot by Love Babbar");
+    }
+}
